4b054c85552057c1408c43683b7a29fb551eca60
Николай Эпаев <dvd11122@mail.ru>
2019-10-24 15:05:56 +0300

    создано более адекватное общее меню

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -8,44 +8,196 @@ using namespace std;
 int main(void)
 {
 	int exit = 0;
+	int exit_create = 0;
+	int exit_create_flat = 0;
+	int exit_create_voluminous = 0;
+	int exit_delete = 0;
 	int c = 0;
-	Flat rectangle;
+	int n = 0;
 	while (exit != 1)
 	{
 		system("cls");
 		cout << "What should we do?" << endl;
-		cout << "1) Set x_size" << endl << "2) Set y_size" << endl << "3) Get x_size" << endl << "4) Get y_size" << endl << "5) Calculate area" << endl << "6) Show area" << endl << "7) exit" << endl;
+		cout << "1) Create new" << endl << "2) Delete" << endl << "3) Save" << endl << "4) Load" << endl << "5) Show Flat" << endl << "6) Show Voluminous" << endl << "7) Exit" << endl;
 		cin >> c;
 		switch (c)
 		{
 		case (1):
 		{
-			rectangle.set_x_size();
+			while (exit_create != 1)
+			{
+				system("cls");
+				cout << "Which element should be created?" << endl;
+				cout << "1) Flat" << endl << "2) Voluminous" << endl << "3) Exit" << endl;
+				cin >> c;
+				switch (c)
+				{
+				case (1):
+				{
+					while (exit_create_flat != 1)
+					{
+						system("cls");
+						cout << "Enter type of element" << endl;
+						cout << "1) Rectangle" << endl << "" << endl << "" << endl << "" << endl << "" << endl << "6) Exit" << endl;
+						cin >> c;
+						switch (c)
+						{
+						case (1):
+						{
+							// вызов создания квадрата
+							break;
+						}
+						case (2):
+						{
+							break;
+						}
+						case (3):
+						{
+							break;
+						}
+						case (4):
+						{
+							break;
+						}
+						case (5):
+						{
+							break;
+						}
+						case (6):
+						{
+							exit_create_flat = 1;
+							break;
+						}
+						default:
+						{
+							cout << "" << endl;
+							system("pause");
+							break;
+						}
+						}
+					}
+					break;
+				}
+				case (2):
+				{
+					while (exit_create_voluminous != 1)
+					{
+						system("cls");
+						cout << "Enter type of element" << endl;
+						cout << "1) Cube" << endl << "" << endl << "" << endl << "" << endl << "" << endl << "6) Exit" << endl;
+						cin >> c;
+						switch (c)
+						{
+						case (1):
+						{
+							// вызов создания куба
+							break;
+						}
+						case (2):
+						{
+							break;
+						}
+						case (3):
+						{
+							break;
+						}
+						case (4):
+						{
+							break;
+						}
+						case (5):
+						{
+							break;
+						}
+						case (6):
+						{
+							exit_create_voluminous = 1;
+							break;
+						}
+						default:
+						{
+							cout << "" << endl;
+							system("pause");
+							break;
+						}
+						}
+					}
+					break;
+				}
+				case (3):
+				{
+					exit_create = 1;
+					break;
+				}
+				default:
+				{
+					cout << "Wrong number" << endl;
+					system("pause");
+					break;
+				}
+				}
+			}
 			break;
 		}
 		case (2):
 		{
-			rectangle.set_y_size();
+			while (exit_delete != 1)
+			{
+				system("cls");
+				cout << "Which element should be deleted?" << endl;
+				cout << "1) Flat" << endl << "2) Voluminous" << endl << "3) Exit" << endl;
+				cin >> c;
+				switch (c)
+				{
+				case (1):
+				{
+					system("cls");
+					cout << "Enter number of element" << endl;
+					// вывод списка плоских элементов
+					cin >> n;
+					// вызов метода удаления по n для плоских элементов
+					break;
+				}
+				case (2):
+				{
+					system("cls");
+					cout << "Enter number of element" << endl;
+					// вывод списка объёмных элементов
+					cin >> n;
+					// вызов метода удаления по n для объёмных элементов
+					break;
+				}
+				case (3):
+				{
+					exit_delete = 1;
+					break;
+				}
+				default:
+					cout << "Wrong number" << endl;
+					system("pause");
+					break;
+				}
+			}
 			break;
 		}
 		case (3):
 		{
-			rectangle.show_x_size();
+			// вызов функции сохранения
 			break;
 		}
 		case (4):
 		{
-			rectangle.show_y_size();
+			// вызов функции восстановления
 			break;
 		}
 		case (5):
 		{
-			rectangle.AreaCalculate();
+			// вызов показа списка плоских фигур
 			break;
 		}
 		case (6):
 		{
-			rectangle.ShowArea();
+			// вызов показа списка объёмных фигур
 			break;
 		}
 		case (7):
@@ -56,6 +208,7 @@ int main(void)
 		default:
 		{
 			cout << "Wrong number" << endl;
+			system("pause");
 			break;
 		}
 		}
