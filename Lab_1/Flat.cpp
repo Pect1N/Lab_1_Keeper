62580913f1eac7557a744d316bdafd4ac967e8ad
Николай Эпаев <dvd11122@mail.ru>
2019-11-18 18:59:17 +0300

    Реализованы методы и поля класса

--- Lab_1/Flat.cpp
+++ Lab_1/Flat.cpp
@@ -1,51 +1,82 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 #include "Flat.h"
 
 using namespace std;
 
-Flat::Flat()
-{
-	next = 0;
-	previous = 0;
-}
-
-Flat::~Flat()
-{
-
-}
-
-void Flat::set_next()
-{
-
-}
-
-void Flat::set_previous()
-{
-
-}
-
-Flat* Flat::get_next()
-{
-
-
-	return 0;
-}
-
-Flat* Flat::get_previous()
-{
-
-
-	return 0;
-}
-
 Rectangle::Rectangle()
 {
-
+	name = 0;
+	ploshad = 0;
+	width = 0;
+	height = 0;
+	picture = 0;
+	cout << "Rectandle constructor" << endl;
 }
 
 Rectangle::~Rectangle()
 {
+	name = 0;
+	ploshad = 0;
+	width = 0;
+	height = 0;
+	picture = 0;
+	cout << "Rectandle destructor" << endl;
+}
 
+void Rectangle::set_name(int name_number)
+{
+	name = name_number;
+}
+
+void Rectangle::set_ploshad()
+{
+	ploshad = width * height;
+}
+
+void Rectangle::set_width(int width_size)
+{
+	width = width_size;
+}
+
+void Rectangle::set_height(int height_size)
+{
+	height = height_size;
+}
+
+void Rectangle::set_picture()
+{
+	picture = name;
+}
+
+int Rectangle::get_name()
+{
+	return name;
+}
+
+int Rectangle::get_ploshad()
+{
+	return ploshad;
+}
+
+int Rectangle::get_width()
+{
+	return width;
+}
+
+int Rectangle::get_height()
+{
+	return height;
+}
+
+int Rectangle::get_picture()
+{
+	return picture;
+}
+
+void Rectangle::show()
+{
+	if (name == 1)
+	{
+		cout << "Rectangle" << endl;
+	}
 }
--- Lab_1/Flat.h
+++ Lab_1/Flat.h
@@ -1,23 +1,35 @@
-#pragma once
-#include <stdio.h>
+#include <iostream>
+#include "Figures.h"
 
-class Flat
+using namespace std;
+
+class Flat : public Figure
 {
-	Flat* next;
-	Flat* previous;
-public:
-	Flat();
-	~Flat();
-	void set_next();
-	void set_previous();
-	Flat* get_next();
-	Flat* get_previous();
+
 };
 
-class Rectangle : Flat
+class Rectangle : public Flat
 {
-
+	int name;
+	int ploshad;
+	int width;
+	int height;
+	int picture;
 public:
+	void A(){}
 	Rectangle();
 	~Rectangle();
+	void set_name(int);
+	void set_ploshad();
+	void set_width(int);
+	void set_height(int);
+	void set_picture();
+	int get_name();
+	int get_ploshad();
+	int get_width();
+	int get_height();
+	int get_picture();
+	void show();
 };
+
+#pragma once
