13a1aead71601adbd651236cd5b4c45564e138ac
Николай Эпаев <dvd11122@mail.ru>
2019-11-18 18:50:44 +0300

    Переработано основное меню

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -1,219 +1,76 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
 #include "Figures.h"
+#include "Flat.h"
+#include "Keeper.h"
 
 using namespace std;
 
 int main(void)
 {
-	int exit = 0;
-	int exit_create = 0;
-	int exit_create_flat = 0;
-	int exit_create_voluminous = 0;
-	int exit_delete = 0;
-	int c = 0;
-	int n = 0;
-	while (exit != 1)
+	Keeper* start;
+	Keeper* finish;
+	char vibor = 0;
+	int vihod = 0;
+
+	while (vihod != 1)
 	{
-		system("cls");
-		cout << "What should we do?" << endl;
-		cout << "1) Create new" << endl << "2) Delete" << endl << "3) Save" << endl << "4) Load" << endl << "5) Show Flat" << endl << "6) Show Voluminous" << endl << "7) Exit" << endl;
-		cin >> c;
-		switch (c)
+		cout << "Chto delat?" << endl;
+		cout << "1) Sozdat" << endl;
+		cout << "2) Redaktirovat" << endl;
+		cout << "3) Udalit" << endl;
+		cout << "4) Sohranit" << endl;
+		cout << "5) Zagruzit" << endl;
+		cout << "6) Pokazat" << endl;
+		cout << "7) Viiti" << endl;
+
+		cin >> vibor;
+
+		switch (vibor)
 		{
-		case (1):
+		case '1':
 		{
-			while (exit_create != 1)
-			{
-				system("cls");
-				cout << "Which element should be created?" << endl;
-				cout << "1) Flat" << endl << "2) Voluminous" << endl << "3) Exit" << endl;
-				cin >> c;
-				switch (c)
-				{
-				case (1):
-				{
-					while (exit_create_flat != 1)
-					{
-						system("cls");
-						cout << "Enter type of element" << endl;
-						cout << "1) Rectangle" << endl << "" << endl << "" << endl << "" << endl << "" << endl << "6) Exit" << endl;
-						cin >> c;
-						switch (c)
-						{
-						case (1):
-						{
-							// вызов создания квадрата
-							break;
-						}
-						case (2):
-						{
-							break;
-						}
-						case (3):
-						{
-							break;
-						}
-						case (4):
-						{
-							break;
-						}
-						case (5):
-						{
-							break;
-						}
-						case (6):
-						{
-							exit_create_flat = 1;
-							break;
-						}
-						default:
-						{
-							cout << "" << endl;
-							system("pause");
-							break;
-						}
-						}
-					}
-					break;
-				}
-				case (2):
-				{
-					while (exit_create_voluminous != 1)
-					{
-						system("cls");
-						cout << "Enter type of element" << endl;
-						cout << "1) Cube" << endl << "" << endl << "" << endl << "" << endl << "" << endl << "6) Exit" << endl;
-						cin >> c;
-						switch (c)
-						{
-						case (1):
-						{
-							// вызов создания куба
-							break;
-						}
-						case (2):
-						{
-							break;
-						}
-						case (3):
-						{
-							break;
-						}
-						case (4):
-						{
-							break;
-						}
-						case (5):
-						{
-							break;
-						}
-						case (6):
-						{
-							exit_create_voluminous = 1;
-							break;
-						}
-						default:
-						{
-							cout << "" << endl;
-							system("pause");
-							break;
-						}
-						}
-					}
-					break;
-				}
-				case (3):
-				{
-					exit_create = 1;
-					break;
-				}
-				default:
-				{
-					cout << "Wrong number" << endl;
-					system("pause");
-					break;
-				}
-				}
-			}
+
 			break;
 		}
-		case (2):
+		case '2':
 		{
-			while (exit_delete != 1)
-			{
-				system("cls");
-				cout << "Which element should be deleted?" << endl;
-				cout << "1) Flat" << endl << "2) Voluminous" << endl << "3) Exit" << endl;
-				cin >> c;
-				switch (c)
-				{
-				case (1):
-				{
-					system("cls");
-					cout << "Enter number of element" << endl;
-					// вывод списка плоских элементов
-					cin >> n;
-					// вызов метода удаления по n для плоских элементов
-					break;
-				}
-				case (2):
-				{
-					system("cls");
-					cout << "Enter number of element" << endl;
-					// вывод списка объёмных элементов
-					cin >> n;
-					// вызов метода удаления по n для объёмных элементов
-					break;
-				}
-				case (3):
-				{
-					exit_delete = 1;
-					break;
-				}
-				default:
-					cout << "Wrong number" << endl;
-					system("pause");
-					break;
-				}
-			}
+
 			break;
 		}
-		case (3):
+		case '3':
 		{
-			// вызов функции сохранения
+
 			break;
 		}
-		case (4):
+		case '4':
 		{
-			// вызов функции восстановления
+
 			break;
 		}
-		case (5):
+		case '5':
 		{
-			// вызов показа списка плоских фигур
+
 			break;
 		}
-		case (6):
+		case '6':
 		{
-			// вызов показа списка объёмных фигур
+
 			break;
 		}
-		case (7):
+		case '7':
 		{
-			exit = 1;
+			vihod = 1;
 			break;
 		}
 		default:
 		{
-			cout << "Wrong number" << endl;
+			cout << "Wrong enter" << endl;
 			system("pause");
 			break;
 		}
 		}
+		system("cls");
 	}
-	cout << "Finished" << endl;
 
 	return 0;
 }
