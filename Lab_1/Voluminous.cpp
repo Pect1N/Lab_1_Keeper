e848ceecf1b54c8a23530ec1f837804740234ea6
PectiN <PectiN@LAPTOP-DVKDDEQ8>
2019-11-29 17:16:52 +0300

    Реализованы все классы объёмных фигур и методы взаимодействия с их полями

---
+++ Lab_1/Voluminous.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <math.h>
+#include "Voluminous.h"
+
+using namespace std;
--- Lab_1/Voluminous.h
+++ Lab_1/Voluminous.h
@@ -1,3 +1,130 @@
+#include <iostream>
+#include "Figures.h"
 
+using namespace std;
+
+class Voluminous : public Figure
+{
+
+};
+
+class Sphere : public Voluminous
+{
+	int name;
+	float ploshad;
+	float radius;
+	int picture;
+public:
+	Sphere();
+	~Sphere();
+	void set_name(int);
+	void set_ploshad();
+	void set_radius(float);
+	void set_picture();
+	int get_name();
+	float get_ploshad();
+	float get_radius();
+	int get_picture();
+	void show();
+	void show_size();
+	int size_change(int);
+};
+
+class Pyramid : public Voluminous
+{
+	int name;
+	float ploshad;
+	float base;
+	float height;
+	int picture;
+public:
+	Pyramid();
+	~Pyramid();
+	void set_name(int);
+	void set_ploshad();
+	void set_base(float);
+	void set_height(float);
+	void set_picture();
+	int get_name();
+	float get_ploshad();
+	float get_base();
+	float get_height();
+	int get_picture();
+	void show();
+	void show_size();
+	int size_change(int);
+};
+
+class Cylinder : public Voluminous
+{
+	int name;
+	float ploshad;
+	float base;
+	float height;
+	int picture;
+public:
+	Cylinder();
+	~Cylinder();
+	void set_name(int);
+	void set_ploshad();
+	void set_base(float);
+	void set_height(float);
+	void set_picture();
+	int get_name();
+	float get_ploshad();
+	float get_base();
+	float get_height();
+	int get_picture();
+	void show();
+	void show_size();
+	int size_change(int);
+};
+
+class Kube : public Voluminous
+{
+	int name;
+	float ploshad;
+	float side;
+	int picture;
+public:
+	Kube();
+	~Kube();
+	void set_name(int);
+	void set_ploshad();
+	void set_side(float);
+	void set_picture();
+	int get_name();
+	float get_ploshad();
+	float get_side();
+	int get_picture();
+	void show();
+	void show_size();
+	int size_change(int);
+};
+
+class Cone : public Voluminous
+{
+	int name;
+	float ploshad;
+	float base;
+	float height;
+	int picture;
+public:
+	Cone();
+	~Cone();
+	void set_name(int);
+	void set_ploshad();
+	void set_base(float);
+	void set_height(float);
+	void set_picture();
+	int get_name();
+	float get_ploshad();
+	float get_base();
+	float get_height();
+	int get_picture();
+	void show();
+	void show_size();
+	int size_change(int);
+};
 
 #pragma once
