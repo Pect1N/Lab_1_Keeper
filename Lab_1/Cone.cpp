556c75c0b30f17135d60119240de2bdd24a97bf5
PectiN <PectiN@LAPTOP-DVKDDEQ8>
2019-11-29 17:18:01 +0300

    Методы класса конуса

---
+++ Lab_1/Cone.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include "Voluminous.h"
+
+using namespace std;
+
+Cone::Cone()
+{
+	name = 0;
+	ploshad = 0;
+	base = 0;
+	height = 0;
+	picture = 0;
+	cout << "Cone constructor" << endl;
+}
+
+Cone::~Cone()
+{
+	name = 0;
+	ploshad = 0;
+	base = 0;
+	height = 0;
+	picture = 0;
+	cout << "Cone destructor" << endl;
+}
+
+void Cone::set_name(int name_number)
+{
+	name = name_number;
+}
+
+void Cone::set_ploshad()
+{
+	ploshad = (float)((base * height) / 3);
+}
+
+void Cone::set_base(float base_size)
+{
+	base = base_size;
+}
+
+void Cone::set_height(float height_size)
+{
+	height = height_size;
+}
+
+void Cone::set_picture()
+{
+	picture = name;
+}
+
+int Cone::get_name()
+{
+	return name;
+}
+
+float Cone::get_ploshad()
+{
+	return ploshad;
+}
+
+float Cone::get_base()
+{
+	return base;
+}
+
+float Cone::get_height()
+{
+	return height;
+}
+
+int Cone::get_picture()
+{
+	return picture;
+}
+
+void Cone::show()
+{
+	cout << "Name: Cone" << endl;
+	cout << "Volume: " << ploshad << endl;
+	cout << "Base: " << base << endl;
+	cout << "Height: " << height << endl;
+	cout << "Picture:" << endl;
+	cout << "   /|\\" << endl;
+	cout << "  / | \\" << endl;
+	cout << " /  |  \\" << endl;
+	cout << "(___|___)" << endl;
+}
+
+void Cone::show_size()
+{
+	cout << "1) Base: " << base << endl;
+	cout << "2) Height: " << height << endl;
+	cout << "3) Exit" << endl;
+}
+
+int Cone::size_change(int parameter)
+{
+	switch (parameter)
+	{
+	case '1':
+	{
+		float new_width;
+		cout << "Enter new base" << endl;
+		cin >> new_width;
+		set_base(new_width);
+		set_ploshad();
+		return 0;
+		break;
+	}
+	case '2':
+	{
+		float new_width;
+		cout << "Enter new height" << endl;
+		cin >> new_width;
+		set_height(new_width);
+		set_ploshad();
+		return 0;
+		break;
+	}
+	case '3':
+	{
+		return 1;
+		break;
+	}
+	default:
+	{
+		cout << "Wrong number" << endl;
+		system("pause");
+		return 0;
+		break;
+	}
+	}
+}
