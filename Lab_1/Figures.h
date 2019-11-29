dab21084d04a31656753c1b0e050a4cf0590da26
PectiN <PectiN@LAPTOP-DVKDDEQ8>
2019-11-29 17:44:26 +0300

    Базовый класс с виртуальными методами

--- Lab_1/Figures.h
+++ Lab_1/Figures.h
@@ -5,18 +5,40 @@ using namespace std;
 class Figure
 {
 public:
-	virtual void A();
 	virtual void set_name(int);
 	virtual void set_ploshad();
-	virtual void set_width(int);
-	virtual void set_height(int);
+	virtual void set_width(float);
+	virtual void set_height(float);
 	virtual void set_picture();
 	virtual int get_name();
-	virtual int get_ploshad();
-	virtual int get_width();
-	virtual int get_height();
+	virtual float get_ploshad();
+	virtual float get_width();
+	virtual float get_height();
 	virtual int get_picture();
 	virtual void show();
+	virtual void show_size();
+	virtual int size_change(int);
+
+	virtual void set_side(float);
+	virtual float get_side();
+
+	virtual void set_radius(float);
+	virtual float get_radius();
+
+	virtual void set_base(float);
+	virtual void set_left(float);
+	virtual void set_right(float);
+	virtual float get_base();
+	virtual float get_left();
+	virtual float get_right();
+
+	virtual void set_top(float);
+	virtual float get_top();
+
+	virtual void set_first_diag(float);
+	virtual void set_second_diag(float);
+	virtual float get_first_diag();
+	virtual float get_second_diag();
 };
 
 #pragma once
